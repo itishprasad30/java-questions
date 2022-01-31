ab15995c1396138e6f54e8e9ec2dcd501595a48d
itishprasad30 <itishprasad20@gmail.com>
2022-01-31 16:29:54 +0530

    finding the next gratest element in array

---
+++ src/nextgreaterelement.java
@@ -0,0 +1,26 @@
+// greater elements in a given array
+class Main {
+    // prints elements and next greater el pair for all elements of arr[] of size n
+
+    static  void  printNextGEL (int arr[] ,int n){
+        int next,i,j;
+        for (  i = 0; i < n; i++) {
+           next = -1;
+            for (j = 0; j < n; j++) {
+                if(arr[i] <arr[j]) {
+                    next= arr[j];
+                    break;
+
+                }
+
+            }
+            System.out.println(arr[i] + "--" +next);
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[] = {11,13,21,2};
+        int n = arr.length;
+        printNextGEL(arr,n);
+    }
+}
